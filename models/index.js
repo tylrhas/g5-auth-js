c1c8e2f0ba25c297d2815ca6eab688229443b01e
Tyler Hasenoehrl <tylrhas@gmail.com>
2020-05-15 11:13:41 -0700

    remove logging

--- models/index.js
+++ models/index.js
@@ -13,7 +13,8 @@ const {
   DATABASE_LOGGING: logging,
   DATABASE_CA: ca,
   DATABASE_CERT: cert,
-  DATABASE_KEY: key
+  DATABASE_KEY: key,
+  GKE: gke
 } = process.env
 
 let minTest = parseInt(min)
@@ -21,17 +22,21 @@ let maxTest = parseInt(max)
 let idleTest = parseInt(idle)
 let acquireTest = parseInt(acquire)
 let evictTest = parseInt(evict) 
+let sslConfig = {}
 
+if (gke === 'true' && ssl === 'true') {
+  sslConfig = { ca, cert, key }
+} else if (ssl === 'true') {
+  sslConfig = {
+    ca: fs.readFileSync(path.join(__dirname,'../../', ca)),
+    cert: fs.readFileSync(path.join(__dirname, '../../', cert)),
+    key: fs.readFileSync(path.join(__dirname,'../../', key))
+  }
+}
 const sequelize = new Sequelize(dbUrl, {
   pool: { maxTest, minTest, idleTest, acquireTest, evictTest },
-  dialectOptions: {
-    ssl: (ssl === 'true') ? {
-      ca: fs.readFileSync(path.join(__dirname,'../../', ca)),
-      cert: fs.readFileSync(path.join(__dirname, '../../', cert)),
-      key: fs.readFileSync(path.join(__dirname,'../../', key))
-    } : false
-  },
-  logging: (logging === 'true')
+  // logging: false,
+  dialectOptions: { ssl : (ssl === 'true') ? sslConfig : {}  }
 })
 
 const db = {}
