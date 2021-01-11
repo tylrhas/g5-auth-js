b9a7ef2d17bb7a7f0fc72ccd766992fb2c9dfba9
Tyler Hasenoehrl <tylrhas@gmail.com>
2021-01-11 13:06:54 -0800

    Update index.js
    update token length

--- index.js
+++ index.js
@@ -15,8 +15,7 @@ function init(app, config) {
   app.use(cookieSession({
     name: 'g5Auth',
     keys: [`${config.session.secret}`],
-    // maxAge: 24 * 60 * 60 * 1000 // 24 hours
-    maxAge: 20 * 60 * 1000  // 1 second
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
   }))
   app.use(passport.initialize())
   app.use(passport.session())
